4ddc1b63c393be78914b132b24452a45d928f525
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2023-08-22 13:18:48 -0600

    1. First commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+const multiply = (a, b) => a * b;
+const a = 10;
+const b = 3;
+console.log(`${a} * ${b} = ${multiply(a, b)}`);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const multiply = (a: number, b: number): number => a * b;
+
+const a = 10;
+const b = 3;
+
+console.log(`${a} * ${b} = ${multiply(a, b)}`);
