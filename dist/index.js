21178c85fc8bbea922562b8c629d0dd883a847b2
Francisco Axel Rojas Lanuza <axlwolf@gmail.com>
2023-08-23 10:36:59 -0600

    2. Classes

--- dist/index.js
+++ dist/index.js
@@ -22,9 +22,101 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Robotin_name, _AdvancedRobot_name;
 Object.defineProperty(exports, "__esModule", { value: true });
 const multiply_1 = __importStar(require("./multiply"));
-const a = 10;
+const a = 25;
 const b = 3;
 console.log(`${a} * ${b} = ${(0, multiply_1.default)(a, b)}`);
 console.log((0, multiply_1.multiplyByTwo)(a));
+var Color;
+(function (Color) {
+    Color["Red"] = "r";
+    Color["Green"] = "g";
+    Color["Blue"] = "b";
+})(Color || (Color = {}));
+let myFavColor = Color.Blue;
+console.log(myFavColor);
+console.log(Color.Red, Color.Green, Color.Blue);
+console.log(Color["Blue"]);
+class Robot {
+    constructor(_name) {
+        this._name = _name;
+        //this.name = name;
+    }
+    askName() {
+        console.log("My name is " + this.name);
+    }
+    move(distance) {
+        console.log(this.name + " moved " + distance + " meters");
+    }
+    set name(value) {
+        this._name = "PREFIX_" + value;
+    }
+    get name() {
+        return this._name + "_SUFFIX";
+    }
+    set color(color) {
+        if (!Robot.isColorAvailable(color)) {
+            throw new Error("Color " + color + " is not available");
+        }
+        this._color = color;
+    }
+}
+Robot.availableColors = ["Red", "Green", "Blue"];
+Robot.isColorAvailable = (color) => Robot.availableColors.includes(color);
+class FlyingRobot extends Robot {
+    constructor(name, jetPackSize) {
+        super(name);
+        this.jetPackSize = jetPackSize;
+    }
+    move(distace) {
+        console.log(this.name + " is flying");
+        super.move(distace);
+    }
+}
+const robot = new Robot("Axel");
+robot.askName();
+const flyingRobot = new FlyingRobot("Francisco Axel", 5);
+flyingRobot.move(500);
+console.log("Flying robot's jetpack size is " + flyingRobot.jetPackSize);
+flyingRobot.name = "Flying robot Axel";
+console.log("My name is " + flyingRobot.name);
+class Robotin {
+    constructor(name) {
+        _Robotin_name.set(this, void 0);
+        this.somePrivateField = "Private!!";
+        __classPrivateFieldSet(this, _Robotin_name, name, "f");
+    }
+    getName() {
+        return __classPrivateFieldGet(this, _Robotin_name, "f");
+    }
+}
+_Robotin_name = new WeakMap();
+class AdvancedRobot extends Robotin {
+    constructor(name) {
+        super(name);
+        _AdvancedRobot_name.set(this, void 0);
+        this.somePrivateField = "Overridden Private!!";
+        __classPrivateFieldSet(this, _AdvancedRobot_name, "Advanced " + name, "f");
+    }
+    getAdvancedRobotName() {
+        return __classPrivateFieldGet(this, _AdvancedRobot_name, "f");
+    }
+}
+_AdvancedRobot_name = new WeakMap();
+const robotin = new AdvancedRobot("Axel");
+console.log("Parent name: " + robotin.getName());
+console.log("subclass name: " + robotin.getAdvancedRobotName());
+console.log("private " + robotin.somePrivateField);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,115 @@
 import multiply, { multiplyByTwo as mBy2, HelloWold } from "./multiply";
 
-const a = 10;
+const a = 25;
 const b = 3;
 
 console.log(`${a} * ${b} = ${multiply(a, b)}`);
 console.log(mBy2(a));
+
+enum Color {
+	Red = "r",
+	Green = "g",
+	Blue = "b",
+}
+
+let myFavColor: Color = Color.Blue;
+
+console.log(myFavColor);
+console.log(Color.Red, Color.Green, Color.Blue);
+console.log(Color["Blue"]);
+
+class Robot {
+	//_name: string;
+	_color!: string;
+	static availableColors: string[] = ["Red", "Green", "Blue"];
+	static isColorAvailable = (color: string) =>
+		Robot.availableColors.includes(color);
+
+	constructor(protected _name: string) {
+		//this.name = name;
+	}
+
+	askName() {
+		console.log("My name is " + this.name);
+	}
+
+	move(distance: number) {
+		console.log(this.name + " moved " + distance + " meters");
+	}
+
+	set name(value: string) {
+		this._name = "PREFIX_" + value;
+	}
+
+	get name() {
+		return this._name + "_SUFFIX";
+	}
+
+	set color(color: string) {
+		if (!Robot.isColorAvailable(color)) {
+			throw new Error("Color " + color + " is not available");
+		}
+		this._color = color;
+	}
+}
+
+class FlyingRobot extends Robot {
+	jetPackSize: number;
+	constructor(name: string, jetPackSize: number) {
+		super(name);
+		this.jetPackSize = jetPackSize;
+	}
+
+	move(distace: number) {
+		console.log(this.name + " is flying");
+		super.move(distace);
+	}
+}
+
+const robot = new Robot("Axel");
+
+robot.askName();
+
+const flyingRobot = new FlyingRobot("Francisco Axel", 5);
+
+flyingRobot.move(500);
+
+console.log("Flying robot's jetpack size is " + flyingRobot.jetPackSize);
+
+flyingRobot.name = "Flying robot Axel";
+
+console.log("My name is " + flyingRobot.name);
+
+class Robotin {
+	#name: string;
+	private somePrivateField: string = "Private!!";
+
+	constructor(name: string) {
+		this.#name = name;
+	}
+
+	getName() {
+		return this.#name;
+	}
+}
+
+class AdvancedRobot extends Robotin {
+	#name: string;
+	private somePrivateField: string = "Overridden Private!!";
+
+	constructor(name: string) {
+		super(name);
+		this.#name = "Advanced " + name;
+	}
+
+	getAdvancedRobotName(): string {
+		return this.#name;
+	}
+}
+
+const robotin = new AdvancedRobot("Axel");
+
+console.log("Parent name: " + robotin.getName());
+console.log("subclass name: " + robotin.getAdvancedRobotName());
+
+console.log("private " + robotin.somePrivateField);
